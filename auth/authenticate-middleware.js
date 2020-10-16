1d99d386d408a4fcac49c4550a5030bd3717497f
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-16 11:13:05 -0500

    added router and middleware code

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken")
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+    if (!token) {
+      return res.status(401).json({ message: "You shall not pass" })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "You shall not pass" })
+      }
+      req.token = decoded
+      next()
+    })
+  }
+  catch (err) {
+    next(err)
+
+  }
 };
